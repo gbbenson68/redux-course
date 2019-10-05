0ad51776d568d254ccc1c180e5cb1a1110ec7aba
gbbenson68 <gbbenson68@gmail.com>
2019-10-05 10:34:43 -0400

    Create constants for actions
    Created constants for actions to make code less error-prone.

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -8,17 +8,21 @@ const initState = {
   currentTodo: ''
 }
 
+// Create constants for actions, to make code less error-prone
+const TODO_ADD = 'TODO_ADD'
+const CURRENT_UPDATE = 'CURRENT_UPDATE'
+
 // Action creator for CURRENT_UPDATE.
-export const updateCurrent = (value) => ({type:'CURRENT_UPDATE', payload: value})
+export const updateCurrent = (value) => ({type:CURRENT_UPDATE, payload: value})
 
 // The actual reducer:
 //    - TODO_ADD - the action for adding a neew todo item to the List
 //    - CURRENT_UPDATE - the action used to re-render the dummy todo field in the form
 export default (state = initState, action) => {
   switch (action.type) {
-    case 'TODO_ADD':
+    case TODO_ADD:
       return {...state, todos: state.todos.concat(action.payload)}
-    case 'CURRENT_UPDATE':
+    case CURRENT_UPDATE:
       return {...state, currentTodo: action.payload}
     default:
       return state
