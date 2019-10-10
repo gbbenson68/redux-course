819bc00835da89ed2b994ed4168a9cf0cc679da2
gbbenson68 <gbbenson68@gmail.com>
2019-10-10 17:39:53 -0400

    Add new replace todo functionality
    - Import updateTodo()
    - create new TODO_REPLACE action
    - create replaceTodo() action creator
    - create toggleTodo() function which toggles isComplete
    - update the state with the updated todo

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,4 +1,4 @@
-import { getTodos, createTodo } from '../lib/todoServices.js'
+import { getTodos, createTodo, updateTodo } from '../lib/todoServices.js'
 import { showMessage } from './messages.js'
 
 const initState = {
@@ -11,11 +11,13 @@ const initState = {
 export const TODO_ADD = 'TODO_ADD'
 export const TODOS_LOAD = 'TODOS_LOAD'
 const CURRENT_UPDATE = 'CURRENT_UPDATE'
+export const TODO_REPLACE = 'TODO_REPLACE'
 
 // Action creators
 export const updateCurrent = (value) => ({type: CURRENT_UPDATE, payload: value})
 export const loadTodos = (todos) => ({type: TODOS_LOAD, payload: todos})
 export const addTodo = (todo) => ({type: TODO_ADD, payload: todo})
+export const replaceTodo = (todo) => ({type: TODO_REPLACE, payload: todo})
 
 // Dispatching functions
 export const fetchTodos = () => {
@@ -28,9 +30,29 @@ export const fetchTodos = () => {
 
 export const saveTodo = (name) => {
   return (dispatch) => {
-    dispatch(showMessage('Saving Todo'))
+    dispatch(showMessage('Saving Todo...'))
     createTodo(name)
       .then(res => dispatch(addTodo(res)))
+  }
+}
+
+export const toggleTodo = (id) => {
+  return (dispatch, getState) => {
+    // Default message for updating a todo
+    dispatch(showMessage('Saving Todo Update...'))
+
+    // Get all todos...
+    const { todos } = getState.todo
+
+    // ... find the one we want to update...
+    const todo = todos.find(todo => todo.id === id)
+
+    // Toggle the isComplete flag on the todo
+    const toggled = { ...todo, isComplete: !todo.isComplete }
+
+    // Perform the update. using the function from todoServices.js
+    updateTodo(toggled)
+      .then(res => dispatch(replaceTodo(res)))
   }
 }
 
@@ -46,6 +68,13 @@ export default (state = initState, action) => {
       return {...state, todos: action.payload}
     case CURRENT_UPDATE:
       return {...state, currentTodo: action.payload}
+    case TODO_REPLACE:
+      return {...state,
+                 todos: state.todos.map(todo => todo.id === action.payload.id
+                                                ? action.payload
+                                                : todo
+                                       )
+             }
     default:
       return state
   }
